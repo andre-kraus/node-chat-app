1005e6e7cef912da106b0934f61482a897c785f3
Andre Kraus <andre.kraus@shopgate.com>
2018-04-13 16:11:40 +0200

    Add geolocation support via geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-let {generateMessage} = require('./message');
+let {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate the correct message object', () => {
@@ -8,6 +8,17 @@ describe('generateMessage', () => {
         const newMessage = generateMessage(from, text);
 
         expect(newMessage).toInclude({from, text});
+        expect(newMessage.createdAt).toBeA('number');
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate the correct location message object', () => {
+        const from = 'From', latitude = 1, longitude = 2;
+        const expectedUrl = `https://www.google.de/maps?q=${latitude},${longitude}`;
+        const locationMessage = generateLocationMessage(from, latitude, longitude);
+
+        expect(locationMessage).toInclude({from, url: expectedUrl});
         expect(newMessage.createdAt).toBeA('number');
     });
 });
