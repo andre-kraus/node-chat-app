76b1d28d130d177105427c0a71d090883ce808c8
Andre Kraus <andre.kraus@shopgate.com>
2018-04-18 13:49:32 +0200

    Formatted time stamps with moment

--- public/js/index.js
+++ public/js/index.js
@@ -9,18 +9,19 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-    console.log('Got new message', message);
-
+    let formattedTime = moment(message.createdAt).format('h:mm a');
     let li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
     jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
     let li = jQuery('<li></li>');
     let a = jQuery('<a target="_blank">My current location</a>');
+    let formattedTime = moment(message.createdAt).format('h:mm a');
 
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedTime}: `);
     a.attr('href', message.url);
     li.append(a);
     jQuery('#messages').append(li);
@@ -48,13 +49,13 @@ locationButton.on('click', function () {
     locationButton.attr('disabled', 'disabled').text('Sending location ...');
 
     navigator.geolocation.getCurrentPosition(function (position) {
-        locationButton.removeAttr('disabled');
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
-        locationButton.removeAttr('disabled').text('Send location ');
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     })
 });
